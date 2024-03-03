fc5deb8f50f8a6a97dcac872484ce1b0511eb06c
youssef khalifi <youssefkhalifi767@gmail.com>
2024-03-03 22:30:12 +0100

    creating app

---
+++ src/main/java/com/basicapp/service/Service.java
@@ -0,0 +1,17 @@
+package com.basicapp.service;
+
+public class Service {
+
+    public int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+}
---
+++ src/test/java/com/basicapp/BasicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
