d28d4a35a6b65d511764e77c825a2554324ef05d
youssef khalifi <youssefkhalifi767@gmail.com>
2024-03-03 22:39:29 +0100

    creating unit test

---
+++ src/test/java/com/basicapp/service/ServiceTest.java
@@ -0,0 +1,32 @@
+package com.basicapp.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    @Test
+    public void testAddition() {
+        Service service = new Service();
+        assertEquals(8, service.addition(5, 3));
+        assertEquals(-2, service.addition(-5, 3));
+        assertEquals(0, service.addition(0, 0));
+    }
+
+    @Test
+    public void testSubtraction() {
+        Service service = new Service();
+        assertEquals(2, service.subtraction(5, 3));
+        assertEquals(-8, service.subtraction(-5, 3));
+        assertEquals(0, service.subtraction(0, 0));
+    }
+
+    @Test
+    public void testMultiplication() {
+        Service service = new Service();
+        assertEquals(15, service.multiplication(5, 3));
+        assertEquals(-15, service.multiplication(-5, 3));
+        assertEquals(0, service.multiplication(0, 0));
+    }
+}
